f130d0329f937c6b30b13993f74d60c55177d972
Miquel Garriga <miquel@entorno.es>
2020-11-20 15:49:22 +0100

    first tests & modifications made to our plugin

--- certbot/examples/plugins/certbot_example_plugins.py
+++ certbot/examples/plugins/certbot_example_plugins.py
@@ -7,18 +7,39 @@ import zope.interface
 
 from certbot import interfaces
 from certbot.plugins import common
+from certbot.plugins import dns_common
 
 
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
-class Authenticator(common.Plugin):
-    """Example Authenticator."""
+class Authenticator(dns_common.DNSAuthenticator):
+    """Example Authenticator for EuroDNS."""
 
-    description = "Example Authenticator plugin"
+    description = "Example Authenticator plugin for EuroDNS"
 
     # Implement all methods from IAuthenticator, remembering to add
     # "self" as first argument, e.g. def prepare(self)...
+    def __init__(self, *args, **kwargs):
+        super(Authenticator, self).__init__(*args, **kwargs)
+        self.credentials = None
 
+    def more_info(self):  # pylint: disable=missing-docstring,no-self-use
+        return (
+            "This plugin configures a DNS CNAME record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
+
+    def _perform(self, domain, validation_name, validation):
+        return (
+            "This plugin configures a DNS CNAME record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
+
+    def _cleanup(self, domain, validation_name, validation):
+        return (
+            "This plugin configures a DNS CNAME record to respond to a dns-01 challenge using "
+            + "the EuroDNS Remote REST API."
+        )
 
 @zope.interface.implementer(interfaces.IInstaller)
 @zope.interface.provider(interfaces.IPluginFactory)
