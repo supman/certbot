91281094f1c6e40c58b8f63d20dd9c5bb763f0a8
Miquel Garriga <miquel@entorno.es>
2020-11-23 14:44:05 +0100

    added add_parser_arguments & _setup_credentials (incomplete)

--- certbot/examples/plugins/certbot_example_plugins.py
+++ certbot/examples/plugins/certbot_example_plugins.py
@@ -3,12 +3,18 @@
 For full examples, see `certbot.plugins`.
 
 """
+
+import logging
+
 import zope.interface
 
 from certbot import interfaces
 from certbot.plugins import common
 from certbot.plugins import dns_common
 
+logger = logging.getLogger(__name__)
+
+ACCOUNT_URL = 'https://docapi.eurodns.com/dns'
 
 @zope.interface.implementer(interfaces.IAuthenticator)
 @zope.interface.provider(interfaces.IPluginFactory)
@@ -23,10 +29,28 @@ class Authenticator(dns_common.DNSAuthenticator):
         super(Authenticator, self).__init__(*args, **kwargs)
         self.credentials = None
 
+    @classmethod
+    def add_parser_arguments(cls, add):  # pylint: disable=arguments-differ
+        super(Authenticator, cls).add_parser_arguments(add, default_propagation_seconds=30)
+        add(
+            "credentials",
+            help="EuroDNS credentials INI file.",
+            default="/etc/letsencrypt/eurodns.ini",
+        )
+
     def more_info(self):  # pylint: disable=missing-docstring,no-self-use
         return (
             "This plugin configures a DNS CNAME record to respond to a dns-01 challenge using "
             + "the EuroDNS Remote REST API."
+        )
+
+    def _setup_credentials(self):
+        self.credentials = self._configure_credentials(
+            'credentials',
+            'EuroDNS credentials INI file',
+            {
+                'token': 'User access token for DNSimple v2 API. (See {0}.)'.format(ACCOUNT_URL)
+            }
         )
 
     def _perform(self, domain, validation_name, validation):
